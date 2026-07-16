94f2ac57a6532ad672f9f467896f354444b917d9
Vlad Sorokin <vladislaw.sorockin@yandex.ru>
2026-07-16 23:59:15 +0300

    remove useless editor

--- src/view/editor/__tests__/editor-view.test.ts
+++
@@ -1,7 +0,0 @@
-import { EditorView } from "../../react/editor/editor-view";
-
-describe("EditorView", () => {
-  it("exports a React component", () => {
-    expect(typeof EditorView).toBe("function");
-  });
-});
